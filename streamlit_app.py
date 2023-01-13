09065284f1c90f2e8f6222eb9ff5c7636ba0f510
Uday Patel <64681968+gabbaar@users.noreply.github.com>
2023-01-13 11:39:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfase Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
